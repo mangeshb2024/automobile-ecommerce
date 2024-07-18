921c7e36a410f3e82ff6a91d01129af237621cb1
Mangesh Bhalerao <mangeshbhalerao@Mangeshs-MacBook-Air.local>
2024-07-18 11:55:56 +0530

    Rendered the navigation bar with log and links

---
+++ src/components/NavBar.tsx
@@ -0,0 +1,24 @@
+import { Heading, HStack, Image } from "@chakra-ui/react";
+import logo from "../assets/CarLogo.jpg";
+
+const NavBar = () => {
+  return (
+    <HStack justify='space-between' paddingRight={10}>
+      <Image src={logo} boxSize="150px" />
+      <a href="https://www.autocarindia.com/car-news">
+        <Heading fontSize={20}>What's New?</Heading>
+      </a>
+      <a href="./index.html">
+        <Heading fontSize={20}>About</Heading>
+      </a>
+      <a href="./index.html">
+        <Heading fontSize={20}>Contact</Heading>
+      </a>
+      <a href="./index.html">
+        <Heading fontSize={20}>Login</Heading>
+      </a>
+    </HStack>
+  );
+};
+
+export default NavBar;
